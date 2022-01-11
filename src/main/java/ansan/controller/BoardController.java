92414aeb4985986d5d2be09ea657d9382072b53f
505 <vxel7408@gmail.com>
2022-01-11 17:03:10 +0900

    123

--- src/main/java/ansan/controller/BoardController.java
+++ src/main/java/ansan/controller/BoardController.java
@@ -45,7 +45,7 @@ public class BoardController {
         //파일처리 [ JSP (COS 라이브러리) ]
         //String dir = "C:\\Users\\505\\Desktop\\springseb01\\src\\main\\resources\\static\\upload";
         String dir = "apps/springseb00/src/main/resources/static/upload";
-        String filepath = dir + "\\" + file.getOriginalFilename();  //저장 경로 + form에서 첨부한 파일이름 호출
+        String filepath = dir + "/" + file.getOriginalFilename();  //저장 경로 + form에서 첨부한 파일이름 호출
         //file.getOriginalFilename(); : form 첨부파일 호출
         file.transferTo(new File(filepath));       //transferTo : 파일 저장 [ 예외 처리 ]
 
